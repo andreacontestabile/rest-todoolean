82a83c70371eaece6d0f9ad4892dea1e23b74e87
Andrea Contestabile <andreacontest@gmail.com>
2020-09-28 17:34:35 +0200

    Modifica e aggiornamento dei todo (update)

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
 $(document).ready(function() {
 
-  //READ
-
-  $.ajax(
-    {
-      "url": "http://157.230.17.132:3013/todos",
-      "method": "GET",
-      "success": function(data) {
-        renderTodoList(data);
-      },
-      "error": function() {
-        alert("Errore");
-      }
-    }
-  );
+  // Carico la lista dei todo con la funzione apposita
+  getTodoList();
 
   // Evento click sul pulsante "Aggiungi"
   $("button#add").click(function() {
     addTodo();
   });
 
+  // Evento pressione tasto INVIO nell'input #text-input
   $("input#text-input").keypress(function(e) {
     if ((e.which == 13) && ($(this).val().length > 0)) {
       addTodo();
@@ -29,7 +18,7 @@ $(document).ready(function() {
   // Evento click sul pulsante "Delete"
   $("ul#todo-list").on("click", "button.delete", function() {
     // Salvo in una variabile il selettore dell'elemento cliccato
-    var thisItem = $(this).parent();
+    var thisItem = $(this).parents("li.todo-item");
     // Salvo l'id dell'elemento corrispondente
     var thisItemId = $(thisItem).attr("id");
 
@@ -38,6 +27,7 @@ $(document).ready(function() {
         "url": "http://157.230.17.132:3013/todos/" + thisItemId,
         "method": "DELETE",
         "success": function() {
+          // Al successo, elimino l'elemento dalla view
           thisItem.remove();
         },
         "error": function() {
@@ -48,7 +38,62 @@ $(document).ready(function() {
 
   });
 
+  // Evento click sul pulsante Edit
+  $("ul#todo-list").on("click", "button.edit", function() {
+    $(this).siblings(".edit-input").fadeToggle();
+  });
+
+  // Evento pressione tasto INVIO nel campo edit-input
+  $("ul#todo-list").on("keypress", ".edit-input", function(e) {
+    if ((e.which == 13) && ($(this).val().length > 0)) {
+      // Salvo il valore dell'input
+      var editInput = $(this).val();
+      console.log(editInput);
+      // Salvo l'elemento corrispondente
+      var thisItem = $(this).parents("li.todo-item")
+      // Salvo l'id dell'elemento
+      var editId = $(thisItem).attr("id");
+
+      $.ajax(
+        {
+          "url": "http://157.230.17.132:3013/todos/" + editId,
+          "method": "PATCH",
+          "data": {
+            "text": editInput
+          },
+          "success": function(data) {
+            // Al successo, svuoto la lista dei todo
+            $("#todo-list").empty();
+            // E la chiedo nuovamente, per aggiornare la view
+            getTodoList();
+          },
+          "error": function() {
+            alert("Errore");
+          }
+        }
+      );
+    }
+  });
+
 });
+
+// Funzione getTodoList
+// Effettua una chiamata get verso l'api ed ottiene la lista dei todo
+function getTodoList() {
+  $.ajax(
+    {
+      "url": "http://157.230.17.132:3013/todos",
+      "method": "GET",
+      "success": function(data) {
+        // Al successo, eseguo la funzione di render della lista
+        renderTodoList(data);
+      },
+      "error": function() {
+        alert("Errore");
+      }
+    }
+  );
+}
 
 // Funzione renderTodoList
 // fa il render della lista dei todo, dopo averla ricevuta dall'api
